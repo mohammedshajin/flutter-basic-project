4a73bba74480e6d401a86d447e9ebd06d5d19cea
mhdshjn <mohammedshajin1997@gmail.com>
2022-06-08 08:52:32 +0530

    container and expanded

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,15 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
+        child: Column(
+          children: [
+            Expanded(
+            child : Container(
+              color: Colors.red,
           width: double.infinity,
           child: Column(
+            mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
                 "Shajin",
@@ -41,18 +47,81 @@ class HomeScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              TextButton(
-                  onPressed: () {
-                    print("Button Clicked");
-                  },
-                  child: Text('Click Me'),),
+              Row(
+                mainAxisAlignment:MainAxisAlignment.center,
+                
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      print("Button Clicked");
+                    },
+                    child: Text('Click Me'),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        print("Button Clicked");
+                      },
+                      icon: Icon(Icons.mic))
+                ],
+              ),
               ElevatedButton(
-                  onPressed: () {
-                    print("Button Clicked");
-                  },
-                  child: Text('Click Me'),)
+                onPressed: () {
+                  print("Button Clicked");
+                },
+                child: Text('Click Me'),
+              )
             ],
           ),
+        ),
+            ),
+            Expanded(child: Container(
+              decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(50), border: Border.all(color: Colors.green, width: 10)),
+            )),
+        Expanded(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Shajin",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 23, 23),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment:MainAxisAlignment.center,
+                  
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        print("Button Clicked");
+                      },
+                      child: Text('Click Me'),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          print("Button Clicked");
+                        },
+                        icon: Icon(Icons.mic))
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Button Clicked");
+                  },
+                  child: Text('Click Me'),
+                )
+              ],
+            ),
+          ),
+        ),
+
+          ],
         ),
       ),
     );
